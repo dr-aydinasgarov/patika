893a5ff4ccfc3c9c7dbb002a8d00453f6c0bb921
Aydin Asgarov <dr.asgarov.aydin@gmail.com>
2026-08-09 09:38:06 +0400

    test7

---
+++ src/Giris/Taksimetre.java
@@ -0,0 +1,24 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+    static void main(String[] args) {
+        int baslangic = 10;
+        double km, kmbas = 2.2, sonuc;
+
+        System.out.println("İndi-Bindi: " + baslangic + "TL");
+        System.out.println("km başına ücret: " + kmbas + "TL");
+
+        Scanner km1 = new Scanner(System.in);
+        System.out.print("Gittiğiniz km: ");
+        km = km1.nextDouble();
+        System.out.println("Toplam km: " + km + " km");
+
+        sonuc = baslangic + km * kmbas;
+
+        boolean TL = sonuc > 20;
+        String adalet = TL ? String.valueOf(sonuc) : "20";
+        System.out.println("Ödenecek tutar: " + adalet + "TL");
+    }
+}
