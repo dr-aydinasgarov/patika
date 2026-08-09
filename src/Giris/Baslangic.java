aef00d86f60b8bfb259c772fc8cfb355ca6e9b64
Aydin Asgarov <dr.asgarov.aydin@gmail.com>
2026-08-09 08:37:30 +0400

    test5

--- src/Giris/Baslangic.java
+++ src/Giris/Baslangic.java
@@ -8,15 +8,32 @@ public class Baslangic {
         Scanner deger = new Scanner(System.in);
         System.out.print("KDV'siz fiyatı giriniz: ");
         fiyat = deger.nextDouble();
-        System.out.println("KDV'siz fiyatınız :" + fiyat);
+
+        boolean kosul1 = fiyat <= 1000;
+        boolean kosul2 = fiyat > 1000;
+
+        double KDVLL;
+        KDVLL = fiyat * 1.18;
+
+        double KDVV;
+        KDVV = fiyat * 0.18;
 
         double KDVL;
-        KDVL = fiyat * 1.18;
-        System.out.println("KDV'li fiyatınız : " + KDVL);
+        KDVL = fiyat * 1.08;
 
         double KDV;
-        KDV = fiyat * 0.18;
-        System.out.println("KDV'niz : " + KDV);
+        KDV = fiyat * 0.08;
+
+        String az1 = kosul1 ? "KDV'li fiyatınız : " + String.valueOf(KDVLL) : "";
+        String az2 = kosul1 ? "KDV'niz : " + String.valueOf(KDVV) : "";
+        String cox1 = kosul2 ? "KDV'li fiyatınız : " + String.valueOf(KDVL)  : "";
+        String cox2 = kosul2 ? "KDV'niz : " + String.valueOf(KDV) : "";
+
+        System.out.println("KDV'siz fiyatınız :" + fiyat);
+        System.out.println(az1);
+        System.out.println(az2);
+        System.out.println(cox1);
+        System.out.println(cox2);
 
 
     }
