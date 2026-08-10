554d1626919e858f0b02507edbed9f5309978515
Aydin Asgarov <dr.asgarov.aydin@gmail.com>
2026-08-10 16:54:14 +0400

    test11

---
+++ src/Giris/Rechnung.java
@@ -0,0 +1,40 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Rechnung {
+    static void main(String[] args) {
+
+        double n1, n2;
+        int ausw;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Geben Sie Ihre Nummer ein: ");
+        n1 = inp.nextDouble();
+
+        System.out.print("Geben Sie Ihre zweite Nummer ein: ");
+        n2 = inp.nextDouble();
+
+        System.out.println("1. Addition\n2. Substraktion\n3. Multiplikation\n4. Division");
+
+        System.out.print("Wahlen Sie bitte aus: ");
+        ausw = inp.nextInt();
+
+        switch (ausw) {
+            case 1:
+                System.out.println("Antwort: " + (n1+n2));
+                break;
+            case 2:
+                System.out.println("Antwort: " + (n1-n2));
+                break;
+            case 3:
+                System.out.println("Antwort: " + (n1*n2));
+                break;
+            case 4:
+                System.out.println("Antwort: " + (n1/n2));
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+}
