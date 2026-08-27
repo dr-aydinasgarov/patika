a22356ea862b384766f299bdc93e24d0fc3c7cd0
Aydin Asgarov <dr.asgarov.aydin@gmail.com>
2026-08-27 22:01:27 +0400

    test28

---
+++ src/Giris/DiamantSterne.java
@@ -0,0 +1,37 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class DiamantSterne {
+    static void main(String[] args) {
+        int n,i,m,k=0,l,s=2;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Geben Sie die Nummer ein: ");
+        n = inp.nextInt();
+
+        for (i=1; i<=n; i++){
+            k++;
+            for(m=n-k; m>0; m--){
+                System.out.print(" ");
+            }
+            for (l=n*2-s; l<n*2-1; l++)
+            System.out.print("*");
+            s+=2;
+            System.out.println("");
+
+        }
+        s-=4;
+        for (i=1; i<n; i++){
+            k--;
+            for(m=n-k; m>0; m--){
+                System.out.print(" ");
+            }
+            for (l=n*2-s; l<n*2-1; l++)
+            System.out.print("*");
+            s-=2;
+            System.out.println("");
+
+        }
+    }
+}
