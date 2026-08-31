53a66d42649c31672fa5ddc96132f1691db0f134
Aydin Asgarov <dr.asgarov.aydin@gmail.com>
2026-08-31 21:18:13 +0400

    test39

---
+++ src/Giris/Recursiv.java
@@ -0,0 +1,24 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Recursiv {
+    static void rec(int a){
+        System.out.print(a + ",");
+        if (a<=0){
+            return;
+        }
+        rec(a-5);
+
+        System.out.print(a + ",");
+    }
+    static void main(String[] args) {
+        int a;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Geben sie die Nummer ein: ");
+        a = inp.nextInt();
+
+        System.out.println("Die Antwort ist: ");
+        rec(a);
+    }
+}
